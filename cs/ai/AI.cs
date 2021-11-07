335ebe5ef987030104baaf7359a649e6cae5cd3b
User <sandsidney3@gmail.com>
2021-11-07 14:16:35 -0500

    complete check square

--- cs/ai/AI.cs
+++ cs/ai/AI.cs
@@ -5,62 +5,58 @@
         //private static int[] possibleMoves;
         public static int[] NextMove(GameMessage gameMessage)
         {
-            var nextMove = new[] { 2, 3 };
+            var nextMove = new[] { -1, -1 };
+            var directions = new[] { 0, 0, 0, 0, 0, 0, 0, 0 };
 
-
-
+            var playerNum = gameMessage.player;
+            //while the move hasnt been found look, unless you cant play(board is full)
+            //check for the oposite players piece
+            for (int j = 0; j < 7; j++)
+                for (int i = 0; i < 7; i++)
+                    if (gameMessage.board[j][i] == 0)
+                        foreach(int element in directions)
+                        {
+                            //check if there is a legal move
+                            LegalMove(j, i, gameMessage, element);
+                            //assign the row and col to move, return 
+                            nextMove[0] = j;
+                            nextMove[1] = i;
+                        }
 
             return nextMove;
         }
 
-        public static int[] CheckMoves(int [] pMove, GameMessage gameMessage)
-            {
 
-            var move  = new[] { 0, 0 };
-            var playerNum = gameMessage.player;
-            //while the move hasnt been found look, unless you cant play(board is full)
-            //check for the oposite players piece
-            for (int j = 0; j < 7; j ++)
-                for(int i = 0; i < 7; i++)
-                    if(gameMessage.board[j][i] == 0)
-                        if (CheckSquare(j, i, gameMessage, 2) == true)
-                        { 
-                            //check if there is a legal move
-
-                                //assign the row and col to move, return 
-                                move[0] = j;
-                                move[1] = i;   
-                        }
-
-            return move;
-
-        }
-
-
-        public static bool CheckSquare(int row, int col, GameMessage message, int playerNum)
+        //check the square surrounding the spot on the board
+        //  return a list of the dirrections the leagal move needs to check 
+        public static int [] CheckSquare(int row, int col, GameMessage message, int [] directions)
         {
-            if (message.board[row - 1][col - 1] == 1 ||
-                message.board[row][col - 1] == 1 ||
-                message.board[row + 1][col - 1] == 1 ||
-                message.board[row - 1][col] == 1 ||
-                message.board[row + 1][col] == 1 ||
-                message.board[row -1][col +1] == 1 ||
-                message.board[row][col + 1] == 1 ||
-                message.board[row + 1][col + 1] == 1)
-                return true;
-            else 
-                return false;
+            
+            if (message.board[row - 1][col - 1] == 1)
+                directions[0] = 1;
+            if (message.board[row -1][col] == 1)
+                directions[1] = 1;
+            if (message.board[row - 1][col + 1] == 1)
+                directions[2] = 1;
+            if (message.board[row][col - 1] == 1)
+                directions[3] = 1;
+            if (message.board[row][col + 1] == 1)
+                directions[4] = 1;
+            if (message.board[row + 1][col - 1] == 1)
+                directions[5] = 1;
+            if (message.board[row + 1][col] == 1)
+                directions[6] = 1;
+            if (message.board[row + 1][col + 1] == 1)
+                directions[7] = 1;
+
+            return directions;
         }
-        public static bool LegalMove(int row, int col, GameMessage message, int playerNum)
+        public static bool LegalMove(int row, int col, GameMessage message, int direction)
         {
-            var i = 0;
-            var j = 0;
+            var c = 0;
+            var r = 0;
 
-            //check horozontal right
-            for (i = col; i < 7; i++)
-                if (message.board[row][i + 1] == 2)
-                    return true;
-
+            
 
             return false;
         }
